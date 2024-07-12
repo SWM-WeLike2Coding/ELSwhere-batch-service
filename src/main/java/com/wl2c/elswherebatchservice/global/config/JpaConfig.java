14c934172434ed81eac70605422b8364fc1577b7
kjungw1025 <kjungw1025@naver.com>
2024-07-12 19:52:06 +0900

    feat: jpa 및 웹 설정 config 추가

---
+++ src/main/java/com/wl2c/elswherebatchservice/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.wl2c.elswherebatchservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
